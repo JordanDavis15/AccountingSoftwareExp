c974bc1a0b86caeb1ac1803e30567573dd9ef387
JordanDavis15 <46542224+JordanDavis15@users.noreply.github.com>
2019-04-08 23:59:57 -0400

    Fixed bat file to account for directory re-organization.

--- RunProgram.bat
+++ RunProgram.bat
@@ -1,3 +1,3 @@
-cd C:\Users\jadav\Documents\JavaProjects\AccountingSoftwareExp\
-java -cp C:\Users\jadav\Documents\JavaProjects\AccountingSoftwareExp\build\classes accounting_software_view.AccountingSoftwareExp
+cd C:\Users\jadav\Documents\My_Programs\JavaProjects\AccountingSoftwareExp\
+java -cp C:\Users\jadav\Documents\My_Programs\JavaProjects\AccountingSoftwareExp\build\classes accounting_software_view.AccountingSoftwareExp
 pause
