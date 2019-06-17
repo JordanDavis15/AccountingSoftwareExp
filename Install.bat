d84c2b1f156299969629b23e04a4417f385858d0
Jordan15 <jadavis7776@yahoo.com>
2019-06-17 11:10:55 -0400

    install batch file now copies accountingData.txt into specified directory

--- Install.bat
+++ Install.bat
@@ -1,6 +1,7 @@
 @echo off
-SET /P inputname= Enter path for installation: 
+SET /P inputname= Enter path for installation(ex. C:\Program Files): 
 xcopy build "%inputname%\AccountingSoftwareExp" /s
+xcopy accountingData.txt "%inputname%\AccountingSoftwareExp" /s
 
 echo cd "%inputname%\AccountingSoftwareExp" >Run.bat
 echo java -cp %inputname%\AccountingSoftwareExp\classes accounting_software_view.AccountingSoftwareExp >> Run.bat
