21fbefaeeb4f970e2cab9f1ce9ba171772444835
JordanDavis15 <46542224+JordanDavis15@users.noreply.github.com>
2019-06-16 20:33:01 -0400

    updated install and run bat files

--- Install.bat
+++ Install.bat
@@ -2,10 +2,7 @@
 SET /P inputname= Enter path for installation: 
 xcopy build "%inputname%\AccountingSoftwareExp" /s
 
-echo cd "%inputname%" >Run.bat
+echo cd "%inputname%\AccountingSoftwareExp" >Run.bat
 echo java -cp %inputname%\AccountingSoftwareExp\classes accounting_software_view.AccountingSoftwareExp >> Run.bat
 
-REM installHELPER.bat
-REM echo cd "c:\Users\%username%\Desktop" >Run.bat
-REM echo java -cp %inputname%\AccountingSoftwareExp\classes accounting_software_view.AccountingSoftwareExp >> Run.bat
 pause
