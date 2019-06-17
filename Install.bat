567c113280b9d7163bbac78e6bf5c10d0ec94762
Jordan15 <jadavis7776@yahoo.com>
2019-06-17 11:25:46 -0400

    need to fix run.bat path in install.bat

--- Install.bat
+++ Install.bat
@@ -3,6 +3,8 @@ SET /P inputname= Enter path for installation(ex. C:\Program Files):
 xcopy build "%inputname%\AccountingSoftwareExp" /s
 xcopy accountingData.txt "%inputname%\AccountingSoftwareExp" /s
 
+rem changes directory to one stored in inputname
+cd "%inputname\AccountingSoftwareExp"
 echo cd "%inputname%\AccountingSoftwareExp" >Run.bat
 echo java -cp %inputname%\AccountingSoftwareExp\classes accounting_software_view.AccountingSoftwareExp >> Run.bat
 
