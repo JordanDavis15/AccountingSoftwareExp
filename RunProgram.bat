26cf04a6eeda618b93e836def44b7308643a634e
JordanDavis15 <46542224+JordanDavis15@users.noreply.github.com>
2019-06-23 20:54:00 -0400

    Made install directions clearer and deleted extra batch files

--- RunProgram.bat
+++
@@ -1,3 +0,0 @@
-cd C:\Users\jadav\Documents\My_Programs\JavaProjects\AccountingSoftwareExp\
-java -cp C:\Users\jadav\Documents\My_Programs\JavaProjects\AccountingSoftwareExp\build\classes accounting_software_view.AccountingSoftwareExp
-pause
