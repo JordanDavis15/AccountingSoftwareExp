c5f8698dd3f2bf09113f09c38ae61d39df1f3f11
Jordan15 <jadavis7776@yahoo.com>
2019-06-25 11:38:00 -0400

    Updated install.bat so that the install does not prompt for input. Need to fix path to move from temp file.

--- Install.bat
+++ Install.bat
@@ -1,21 +1,22 @@
-@echo off
-SET /P inputname= Enter path for installation(ex. C:\Program Files): 
+@echo off 
+
+SET inputname=C:\temp
+
 xcopy build "%inputname%\AccountingSoftwareExp" /s
 xcopy accountingData.txt "%inputname%\AccountingSoftwareExp" /s
 
 rem changes directory to one stored in inputname
 cd "%inputname%\AccountingSoftwareExp"
-echo cd "%inputname%\AccountingSoftwareExp" > Run.bat
-echo java -cp %inputname%\AccountingSoftwareExp\classes accounting_software_view.AccountingSoftwareExp >> Run.bat
+ECHO cd "%inputname%\AccountingSoftwareExp" > Run.bat
+ECHO java -cp %inputname%\AccountingSoftwareExp\classes accounting_software_view.AccountingSoftwareExp >> Run.bat
 
 rem this adds a Desktop shortcut
-echo $WshShell = New-Object -comObject WScript.Shell > makeShortcut.ps1
-echo $Shortcut = $WshShell.CreateShortcut("$Home\Desktop\EasyAccounting.lnk") >> makeShortcut.ps1
-echo $Shortcut.TargetPath = "%inputname%\AccountingSoftwareExp\run.bat" >> makeShortcut.ps1
-echo $Shortcut.Save() >> makeShortcut.ps1
+ECHO $WshShell = New-Object -comObject WScript.Shell > makeShortcut.ps1
+ECHO $Shortcut = $WshShell.CreateShortcut("$Home\Desktop\EasyAccounting.lnk") >> makeShortcut.ps1
+ECHO $Shortcut.TargetPath = "%inputname%\AccountingSoftwareExp\run.bat" >> makeShortcut.ps1
+ECHO $Shortcut.Save() >> makeShortcut.ps1
 PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "& './makeShortcut.ps1'"
 del makeShortcut.ps1
 
-echo A shortcut has been placed on your desktop
-
-pause
+ECHO A shortcut has been placed on your desktop
+PAUSE
