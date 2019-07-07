d5e3920f2e1bdc40ec6a3b1cc83ac173de228b3e
JordanDavis15 <46542224+JordanDavis15@users.noreply.github.com>
2019-07-07 19:05:39 -0400

    now able to download and install in single batch file (InstallV_2.bat)

--- InstallV_2.bat
+++ InstallV_2.bat
@@ -1,10 +1,25 @@
 @ECHO off
-SET tempPlace=C:\temp
-SET destPlace=C:\Users\%USERNAME%\Documents
+SET tempPlace=C:\temp\AccountingSoftwareExp
+SET destPlace=C:\Users\%USERNAME%\Documents\AccountingSoftwareExp
 
 cd tempPlace
-git clone https://github.com/JordanDavis15/AccountingSoftwareExp.git
+git clone https://github.com/JordanDavis15/AccountingSoftwareExp.git %tempPlace%
 
-xcopy build "%destPlace%\AccountingSoftwareExp" /s
-IF EXIST "destPlace\AccountingSoftwareExp\acountingData.txt" ECHO data already exists...will not overwrite
-ELSE xcopy accountingData.txt "%destPlace%\AccountingSoftwareExp" /s
+xcopy build "%destPlace%" /s /y
+IF EXIST "%destPlace%\accountingData.txt" (
+	ECHO data already exists...will not overwrite
+)ELSE (
+	xcopy accountingData.txt "%destPlace%" /s
+)
+RMDIR /Q/S %tempPlace%
+
+rem this adds a Desktop shortcut
+ECHO $WshShell = New-Object -comObject WScript.Shell > makeShortcut.ps1
+ECHO $Shortcut = $WshShell.CreateShortcut("$Home\Desktop\EasyAccounting.lnk") >> makeShortcut.ps1
+ECHO $Shortcut.TargetPath = "%destPlace%\AccountingSoftwareExp\run.bat" >> makeShortcut.ps1
+ECHO $Shortcut.Save() >> makeShortcut.ps1
+PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "& './makeShortcut.ps1'"
+del makeShortcut.ps1
+ECHO A shortcut has been placed on your desktop
+
+PAUSE
