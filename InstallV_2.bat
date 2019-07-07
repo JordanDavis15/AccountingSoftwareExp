f8a04b311e5e14185cd71820cb61c315b44454c9
JordanDavis15 <46542224+JordanDavis15@users.noreply.github.com>
2019-07-07 19:26:35 -0400

    updated installV_2.bat

--- InstallV_2.bat
+++ InstallV_2.bat
@@ -1,22 +1,26 @@
 @ECHO off
-SET tempPlace=C:\temp\AccountingSoftwareExp
+SET tempPlace="C:\temp\AccountingSoftwareExp"
 SET destPlace=C:\Users\%USERNAME%\Documents\AccountingSoftwareExp
 
-cd tempPlace
 git clone https://github.com/JordanDavis15/AccountingSoftwareExp.git %tempPlace%
 
-xcopy build "%destPlace%" /s /y
+xcopy "%tempPlace%\build" "%destPlace%" /s /y
 IF EXIST "%destPlace%\accountingData.txt" (
 	ECHO data already exists...will not overwrite
 )ELSE (
-	xcopy accountingData.txt "%destPlace%" /s
+	xcopy "%tempPlace%\accountingData.txt" "%destPlace%" /s
 )
 RMDIR /Q/S %tempPlace%
+
+rem creates run.bat
+cd %destPlace%
+ECHO cd "%destPlace%" > Run.bat
+ECHO java -cp "%destPlace%\classes accounting_software_view.AccountingSoftwareExp" >> Run.bat
 
 rem this adds a Desktop shortcut
 ECHO $WshShell = New-Object -comObject WScript.Shell > makeShortcut.ps1
 ECHO $Shortcut = $WshShell.CreateShortcut("$Home\Desktop\EasyAccounting.lnk") >> makeShortcut.ps1
-ECHO $Shortcut.TargetPath = "%destPlace%\AccountingSoftwareExp\run.bat" >> makeShortcut.ps1
+ECHO $Shortcut.TargetPath ="%destPlace%\run.bat" >> makeShortcut.ps1
 ECHO $Shortcut.Save() >> makeShortcut.ps1
 PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "& './makeShortcut.ps1'"
 del makeShortcut.ps1
