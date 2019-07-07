d1d3a1172dd2a7b8d8576b39361e352a9ff400b6
JordanDavis15 <46542224+JordanDavis15@users.noreply.github.com>
2019-07-07 17:53:40 -0400

    updated final destination of class files and run.bat

--- Install.bat
+++ Install.bat
@@ -1,19 +1,20 @@
 @echo off 
 
-SET inputname=C:\temp
+SET tempPlace=C:\temp
+SET destPlace=C:\Users\%USERNAME%\Documents
 
-xcopy build "%inputname%\AccountingSoftwareExp" /s
-xcopy accountingData.txt "%inputname%\AccountingSoftwareExp" /s
+xcopy build "%destPlace%\AccountingSoftwareExp" /s
+xcopy accountingData.txt "%destPlace%\AccountingSoftwareExp" /s
 
-rem changes directory to one stored in inputname
-cd "%inputname%\AccountingSoftwareExp"
-ECHO cd "%inputname%\AccountingSoftwareExp" > Run.bat
-ECHO java -cp %inputname%\AccountingSoftwareExp\classes accounting_software_view.AccountingSoftwareExp >> Run.bat
+rem changes directory to one stored in destPlace
+cd "%destPlace%\AccountingSoftwareExp"
+ECHO cd "%destPlace%\AccountingSoftwareExp" > Run.bat
+ECHO java -cp %destPlace%\AccountingSoftwareExp\classes accounting_software_view.AccountingSoftwareExp >> Run.bat
 
 rem this adds a Desktop shortcut
 ECHO $WshShell = New-Object -comObject WScript.Shell > makeShortcut.ps1
 ECHO $Shortcut = $WshShell.CreateShortcut("$Home\Desktop\EasyAccounting.lnk") >> makeShortcut.ps1
-ECHO $Shortcut.TargetPath = "%inputname%\AccountingSoftwareExp\run.bat" >> makeShortcut.ps1
+ECHO $Shortcut.TargetPath = "%destPlace%\AccountingSoftwareExp\run.bat" >> makeShortcut.ps1
 ECHO $Shortcut.Save() >> makeShortcut.ps1
 PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "& './makeShortcut.ps1'"
 del makeShortcut.ps1
